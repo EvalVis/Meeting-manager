c5466523dde273b477298ee477a1eb7518234bca
Evaldas Visockas <vievaldas@gmail.com>
2022-07-12 17:39:42 +0300

    Create meeting with file handler, add helper methods for dealing with json

--- src/main/java/ev/projects/meetingmanager/handlers/FileHandler.java
+++ src/main/java/ev/projects/meetingmanager/handlers/FileHandler.java
@@ -1,12 +1,20 @@
 package ev.projects.meetingmanager.handlers;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import ev.projects.meetingmanager.models.MeetingBinding;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class FileHandler {
 
     File dataFile;
+    private final Type listType = new TypeToken<List<MeetingBinding>>() {}.getType();
 
     public FileHandler() {
         createFileIfNotFound();
@@ -22,6 +30,27 @@ public class FileHandler {
                 e.printStackTrace();
             }
         }
+    }
+
+    public void createMeeting(MeetingBinding meetingBinding) {
+        List<MeetingBinding> meetingBindings = getMeetingsFromFile();
+        meetingBindings.add(meetingBinding);
+        writeObjectToFile(meetingBindings, listType);
+    }
+
+    private List<MeetingBinding> getMeetingsFromFile() {
+        List<MeetingBinding> meetings = null;
+        try(JsonReader reader = new JsonReader(new FileReader(dataFile))) {
+            meetings = new Gson().fromJson(reader, listType);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        return meetings;
+    }
+
+    private <T> void writeObjectToFile(T data, Type dataType) {
+        String jsonData = new Gson().toJson(data, dataType);
+        writeToFile(jsonData);
     }
 
     private void writeToFile(String data) {
